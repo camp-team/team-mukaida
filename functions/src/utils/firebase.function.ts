51f564f927dc151a763b73c415e5f4d3dcfbea36
Yusuke Gunji <inochideutae@gmail.com>
2021-01-23 22:04:35 +0900

    feat(firebase): べき等追加

--- functions/src/utils/firebase.function.ts
+++ functions/src/utils/firebase.function.ts
@@ -53,3 +53,31 @@ function deleteQueryBatch(
     })
     .catch(reject);
 }
+
+export function markEventTried(
+  eventId: string
+): Promise<FirebaseFirestore.WriteResult> {
+  const documentRef = db.collection('functionEvents').doc(eventId);
+  return documentRef.set({ tried: true });
+}
+
+const leaseTime = 60 * 1000;
+
+export function shouldEventRun(eventId: string): Promise<boolean> {
+  const documentRef = db.collection('functionEvents').doc(eventId);
+  return db.runTransaction((transaction) => {
+    return transaction.get(documentRef).then((doc) => {
+      const data = doc.data();
+      if (doc.exists && data && data.tried) {
+        return false;
+      }
+      if (doc.exists && data && new Date() < data.lease) {
+        return Promise.reject('Lease already taken, try later.');
+      }
+      transaction.set(documentRef, {
+        lease: new Date(new Date().getTime() + leaseTime),
+      });
+      return true;
+    });
+  });
+}

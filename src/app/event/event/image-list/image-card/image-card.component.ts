a5a229f9b80c0d4fd46b648a3918a1034fb4b4d4
Yusuke Gunji <inochideutae@gmail.com>
2021-02-20 17:33:25 +0900

    feat(image-card): add comments$

--- src/app/event/event/image-list/image-card/image-card.component.ts
+++ src/app/event/event/image-list/image-card/image-card.component.ts
@@ -1,11 +1,15 @@
 import { Component, Input, OnInit } from '@angular/core';
 import { MatDialog } from '@angular/material/dialog';
 import { MatSnackBar } from '@angular/material/snack-bar';
+import { ActivatedRoute } from '@angular/router';
 import { Observable } from 'rxjs';
+import { map, switchMap } from 'rxjs/operators';
+import { Comment } from 'src/app/interfaces/comment';
 import { Event } from 'src/app/interfaces/event';
 import { Image } from 'src/app/interfaces/image';
 import { User } from 'src/app/interfaces/user';
 import { AuthService } from 'src/app/services/auth.service';
+import { CommentService } from 'src/app/services/comment.service';
 import { ImageService } from 'src/app/services/image.service';
 import { DeleteDialogComponent } from 'src/app/shared/delete-dialog/delete-dialog.component';
 
@@ -17,29 +21,43 @@ import { DeleteDialogComponent } from 'src/app/shared/delete-dialog/delete-dialo
 export class ImageCardComponent implements OnInit {
   @Input() image: Image;
   @Input() event: Event;
-
-  comments: any[] = [
-    {
-      coment: '',
-    },
-    {
-      coment: '',
-    },
-    {
-      coment: '',
-    },
-  ];
+  comments: Comment[];
 
   user$: Observable<User> = this.authService.user$;
+
+  eventId$: Observable<string> = this.route.paramMap.pipe(
+    map((param) => {
+      return param.get('eventId');
+    })
+  );
+
+  imageList$: Observable<Image[]> = this.eventId$.pipe(
+    switchMap((eventId) => {
+      return this.imageService.getImages(eventId);
+    })
+  );
+
+  comments$: Observable<Comment[]> = this.eventId$.pipe(
+    switchMap((params) => {
+      const eventId = params;
+      return this.commentService.getComments(eventId, this.image.imageId);
+    })
+  );
 
   constructor(
     private imageService: ImageService,
     private dialog: MatDialog,
     private snackBar: MatSnackBar,
-    private authService: AuthService
+    private authService: AuthService,
+    private route: ActivatedRoute,
+    private commentService: CommentService
   ) {}
 
-  ngOnInit(): void {}
+  ngOnInit(): void {
+    this.comments$.subscribe((comments) => {
+      this.comments = comments;
+    });
+  }
 
   isEditMode() {}
 

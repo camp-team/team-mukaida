58acc174c1aa1256915125bf783572a26733cae1
peyo-code <mr233425@gmail.com>
2021-02-20 18:24:28 +0900

    feat(like.function):べき等でデータを守る。

--- functions/src/like.function.ts
+++ functions/src/like.function.ts
@@ -1,21 +1,36 @@
 import * as admin from 'firebase-admin';
+import { shouldRun, markTried } from 'shouldEventRun';
 const db = admin.firestore();
 const functions = require('firebase-functions');
 
 // カウントアップ
-export const countUpLiked = functions
-  .document(`events/{eventId}/images/{likedUid}`)
+export const countUpLiked = functions.firestore
+  .document('items/{itemId}/likedUserIds/{userId}')
   .onCreate(async (snap: any, context: any) => {
-    return db
-      .doc(`events/${context.params.eventId}`)
-      .update('likedCount', admin.firestore.FieldValue.increment(1));
+    const eventId = context.eventId;
+    return shouldRun(eventId).then((should: any) => {
+      if (should) {
+        db.doc(`items/${context.params.itemId}`).update(
+          'likedCount',
+          admin.firestore.FieldValue.increment(1)
+        );
+        return markTried(eventId);
+      }
+    });
   });
 
 // カウントダウン
 export const countDownLiked = functions
   .document(`events/{eventId}/images/{likedUid}`)
   .onDelete(async (snap: any, context: any) => {
-    return db
-      .doc(`items/${context.params.itemId}`)
-      .update('likedCount', admin.firestore.FieldValue.increment(-1));
+    const eventId = context.eventId;
+    return shouldRun(eventId).then((should: any) => {
+      if (should) {
+        db.doc(`items/${context.params.itemId}`).update(
+          'likedCount',
+          admin.firestore.FieldValue.increment(-1)
+        );
+        return markTried(eventId);
+      }
+    });
   });

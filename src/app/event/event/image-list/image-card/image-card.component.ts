79402d489182b3fffadf0f73ff3221d0b19a9aa1
Yusuke Gunji <inochideutae@gmail.com>
2021-02-20 16:27:32 +0900

    feat(image-card): add comments$

--- src/app/event/event/image-list/image-card/image-card.component.ts
+++ src/app/event/event/image-list/image-card/image-card.component.ts
@@ -1,11 +1,15 @@
 import { Component, Input, OnInit } from '@angular/core';
 import { MatDialog } from '@angular/material/dialog';
 import { MatSnackBar } from '@angular/material/snack-bar';
+import { ActivatedRoute } from '@angular/router';
 import { Observable } from 'rxjs';
+import { map, switchMap } from 'rxjs/operators';
+import { Comment } from 'src/app/interfaces/comment';
 import { Event } from 'src/app/interfaces/event';
 import { Image } from 'src/app/interfaces/image';
 import { User } from 'src/app/interfaces/user';
 import { AuthService } from 'src/app/services/auth.service';
+import { CommentService } from 'src/app/services/comment.service';
 import { ImageService } from 'src/app/services/image.service';
 import { DeleteDialogComponent } from 'src/app/shared/delete-dialog/delete-dialog.component';
 
@@ -17,18 +21,29 @@ import { DeleteDialogComponent } from 'src/app/shared/delete-dialog/delete-dialo
 export class ImageCardComponent implements OnInit {
   @Input() image: Image;
   @Input() event: Event;
+  comments: Comment[];
 
-  comments: any[] = [
-    {
-      coment: '',
-    },
-    {
-      coment: '',
-    },
-    {
-      coment: '',
-    },
-  ];
+  eventId$: Observable<string> = this.route.paramMap.pipe(
+    map((param) => {
+      return param.get('eventId');
+    })
+  );
+
+  imageList$: Observable<Image[]> = this.eventId$.pipe(
+    switchMap((id) => {
+      return this.imageService.getImages(id);
+    })
+  );
+
+  comments$: Observable<Comment[]> = this.eventId$.pipe(
+    switchMap((params) => {
+      const eventId = params;
+      console.log(this.image.imageId);
+      console.log(eventId);
+
+      return this.commentService.getComments(eventId, this.image.imageId);
+    })
+  );
 
   user$: Observable<User> = this.authService.user$;
 
@@ -36,10 +51,16 @@ export class ImageCardComponent implements OnInit {
     private imageService: ImageService,
     private dialog: MatDialog,
     private snackBar: MatSnackBar,
-    private authService: AuthService
+    private authService: AuthService,
+    private commentService: CommentService,
+    private route: ActivatedRoute
   ) {}
 
-  ngOnInit(): void {}
+  ngOnInit(): void {
+    this.comments$.subscribe((comments) => {
+      this.comments = comments;
+    });
+  }
 
   isEditMode() {}
 

ade354a72e7d45b0c55961d81c18b980d05e8df7
Yusuke Gunji <inochideutae@gmail.com>
2021-03-18 23:52:59 +0900

    feat(event): add video-player

---
+++ src/app/event/event/image-detail/video-player/video-player.component.ts
@@ -0,0 +1,49 @@
+import {
+  Component,
+  ElementRef,
+  Input,
+  OnDestroy,
+  OnInit,
+  ViewChild,
+} from '@angular/core';
+import videojs from 'video.js';
+
+@Component({
+  selector: 'app-video-player',
+  templateUrl: './video-player.component.html',
+  styleUrls: ['./video-player.component.scss'],
+})
+export class VideoPlayerComponent implements OnInit, OnDestroy {
+  @ViewChild('target', { static: true }) target: ElementRef;
+  // see options: https://github.com/videojs/video.js/blob/maintutorial-options.html
+  @Input() options: {
+    fluid: boolean;
+    responsive: boolean;
+    aspectRatio: string;
+    autoplay: boolean;
+    sources: {
+      src: string;
+      type: string;
+    }[];
+  };
+  player: videojs.Player;
+
+  constructor(private elementRef: ElementRef) {}
+
+  ngOnInit(): void {
+    this.player = videojs(
+      this.target.nativeElement,
+      this.options,
+      function onPlayerReady() {
+        console.log('onPlayerReady', this);
+      }
+    );
+  }
+
+  ngOnDestroy() {
+    // destroy player
+    if (this.player) {
+      this.player.dispose();
+    }
+  }
+}

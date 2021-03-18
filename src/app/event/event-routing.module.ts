27daa8d72015f81647e7c49f969f03bd8d9eb9ef
Yusuke Gunji <inochideutae@gmail.com>
2021-03-18 23:59:26 +0900

    feat(routing): add post-video routing

--- src/app/event/event-routing.module.ts
+++ src/app/event/event-routing.module.ts
@@ -5,6 +5,7 @@ import { EditComponent } from './edit/edit.component';
 import { EventComponent } from './event/event.component';
 import { ImageDetailComponent } from './event/image-detail/image-detail.component';
 import { PostImagesComponent } from './post-images/post-images.component';
+import { PostVideoComponent } from './post-video/post-video.component';
 
 const routes: Routes = [
   {
@@ -23,6 +24,10 @@ const routes: Routes = [
   {
     path: 'images/:imageId',
     component: ImageDetailComponent,
+  },
+  {
+    path: 'post-video',
+    component: PostVideoComponent,
   },
 ];
 

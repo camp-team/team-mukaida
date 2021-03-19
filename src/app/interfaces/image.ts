ac44c65abe85fcec10ba78add565592096b58c09
Yusuke Gunji <inochideutae@gmail.com>
2021-03-19 23:49:13 +0900

    feat(interfaces): add post

--- src/app/interfaces/image.ts
+++ src/app/interfaces/image.ts
@@ -2,14 +2,13 @@ import { Data } from '@angular/router';
 import { User } from './user';
 
 export interface Image {
-  imageId?: string;
-  uid?: string;
+  imageId: string;
+  uid: string;
   imageURL: string;
-  eventId?: string;
-  createAt?: Data;
+  eventId: string;
+  createdAt: firebase.default.firestore.Timestamp;
   likedUid?: string;
   likedCount?: string;
-  comment?: string;
 }
 
 export interface ImageWithUser extends Image {

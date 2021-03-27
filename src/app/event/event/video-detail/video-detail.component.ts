add4496869270502ba8d7560dfd1cb741abbeb6b
Yusuke Gunji <inochideutae@gmail.com>
2021-03-28 02:19:24 +0900

    feat(video-detail): add like video

--- src/app/event/event/video-detail/video-detail.component.ts
+++ src/app/event/event/video-detail/video-detail.component.ts
@@ -1,9 +1,11 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { Observable } from 'rxjs';
-import { switchMap } from 'rxjs/operators';
+import { switchMap, take } from 'rxjs/operators';
 import { User } from 'src/app/interfaces/user';
 import { Video } from 'src/app/interfaces/video';
+import { AuthService } from 'src/app/services/auth.service';
+import { LikedService } from 'src/app/services/liked.service';
 import { UserService } from 'src/app/services/user.service';
 import { VideoService } from 'src/app/services/video.service';
 
@@ -13,6 +15,11 @@ import { VideoService } from 'src/app/services/video.service';
   styleUrls: ['./video-detail.component.scss'],
 })
 export class VideoDetailComponent implements OnInit {
+  imageCreatedAt: any;
+  uid: string;
+  likedCount: number;
+  isLiked: boolean;
+  imageIds = [];
   eventId: string = this.route.snapshot.paramMap.get('eventId');
   videoId: string = this.route.snapshot.paramMap.get('videoId');
   video$: Observable<Video> = this.videoService.getVideo(
@@ -29,8 +36,36 @@ export class VideoDetailComponent implements OnInit {
   constructor(
     private videoService: VideoService,
     private route: ActivatedRoute,
-    private userService: UserService
+    private userService: UserService,
+    private likedService: LikedService,
+    private authService: AuthService
   ) {}
 
-  ngOnInit(): void {}
+  ngOnInit(): void {
+    this.likedService
+      .isLiked(this.eventId, this.authService.uid, this.videoId)
+      .pipe(take(1))
+      .subscribe((isLiked) => {
+        this.isLiked = isLiked;
+      });
+    this.likedService
+      .getLikedCount(this.eventId, this.videoId)
+      .pipe(take(1))
+      .subscribe((likedCount) => {
+        this.likedCount = likedCount.length;
+      });
+  }
+
+  likeVideo(videoId: string) {
+    this.isLiked = true;
+    this.likedCount++;
+    this.imageIds.push(videoId);
+    this.likedService.likeItem(this.eventId, this.authService.uid, videoId);
+  }
+
+  UnLikeVideo(videoId: string) {
+    this.isLiked = false;
+    this.likedCount--;
+    this.likedService.unlike(this.eventId, this.authService.uid, videoId);
+  }
 }

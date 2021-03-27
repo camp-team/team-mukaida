c8b383d336f02ecaa03aea54045f6b7f7bb13c77
peyo-code <mr233425@gmail.com>
2021-03-27 15:53:30 +0900

    feat(image-detail):いいねと画像投稿日時の表示

--- src/app/event/event/image-detail/image-detail.component.ts
+++ src/app/event/event/image-detail/image-detail.component.ts
@@ -1,11 +1,13 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 import { ImageService } from 'src/app/services/image.service';
 import { ActivatedRoute } from '@angular/router';
 import { Observable } from 'rxjs';
 import { Image } from 'src/app/interfaces/image';
 import { User } from 'src/app/interfaces/user';
-import { switchMap } from 'rxjs/operators';
+import { switchMap, take } from 'rxjs/operators';
 import { UserService } from 'src/app/services/user.service';
+import { LikedService } from 'src/app/services/liked.service';
+import { AuthService } from 'src/app/services/auth.service';
 
 @Component({
   selector: 'app-image-detail',
@@ -13,6 +15,12 @@ import { UserService } from 'src/app/services/user.service';
   styleUrls: ['./image-detail.component.scss'],
 })
 export class ImageDetailComponent implements OnInit {
+  @Input() image: Image;
+  imageCreatedAt: any;
+  uid: string;
+  likedCount: number;
+  isLiked: boolean;
+  imageIds = [];
   eventId: string = this.route.snapshot.paramMap.get('eventId');
   imageId: string = this.route.snapshot.paramMap.get('imageId');
   image$: Observable<Image> = this.imageService.getImage(
@@ -25,12 +33,44 @@ export class ImageDetailComponent implements OnInit {
       return this.userService.getUserData(uid);
     })
   );
-
   constructor(
     private imageService: ImageService,
     private route: ActivatedRoute,
-    private userService: UserService
+    private userService: UserService,
+    private likedService: LikedService,
+    private authService: AuthService
   ) {}
 
-  ngOnInit(): void {}
+  ngOnInit(): void {
+    // 自分がいいねをしたか、していないかを判定する。
+    console.log();
+    this.likedService
+      .isLiked(this.eventId, this.imageId, this.authService.uid)
+      .pipe(take(1))
+      .subscribe((isLiked) => {
+        this.isLiked = isLiked;
+      });
+    // 各画像のいいね数を取得する。
+    this.likedService
+      .getLikedCount(this.eventId, this.imageId)
+      .pipe(take(1))
+      .subscribe((likedCount) => {
+        console.log(likedCount);
+        this.likedCount = likedCount.length;
+      });
+  }
+
+  likeImage(imageId: string) {
+    console.log(imageId);
+    this.isLiked = true;
+    this.likedCount++;
+    this.imageIds.push(imageId);
+    this.likedService.likeItem(this.eventId, imageId, this.authService.uid);
+  }
+
+  UnLikeImage(imageId: string) {
+    this.isLiked = false;
+    this.likedCount--;
+    this.likedService.unlike(this.eventId, imageId, this.authService.uid);
+  }
 }
